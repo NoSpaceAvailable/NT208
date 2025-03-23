5835860227cc9b221bbc36c30cadbbbb8b0c43eb
NoSpaceAvailable <23520197@gm.uit.edu.vn>
2025-03-23 16:07:32 +0700

    add oauth v2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,19 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import NavBar from './components/NavBar.vue';
 import Footer from './components/Footer.vue';
+import vue3GoogleLogin from 'vue3-google-login';
+
 import router from './router/router';
+
+const gauthOption = {
+    clientId: '718649155809-pvn7nnt5m98em3h2svvgidkvteh6pkik.apps.googleusercontent.com',
+    scope: 'profile email',
+    prompt: 'select_account'
+};
 
 createApp(NavBar).mount('#nav');
 createApp(App)
     .use(router)
+    .use(vue3GoogleLogin, gauthOption)
     .mount('#app');
 createApp(Footer).mount('#footer');
